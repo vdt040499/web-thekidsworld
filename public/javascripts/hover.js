6207e474ba5625b2002b5c345ad3077dcfd37fec
VoTan <vdt040499@gmail.com>
2020-06-04 18:04:39 +0700

    Update sigin signup layout

---
+++ public/javascripts/hover.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.card').hover(function(){
+        $(this).animate({
+            marginTop : "-=1%",
+        },200);
+    },
+        function(){
+            $(this).animate({
+                marginTop: "0%"
+            },200);
+        }
+    );
+});
