a05daa408165800804c308f67a9bfd8d86e139db
VoTan <vdt040499@gmail.com>
2020-06-13 12:44:29 +0700

    Update index signup signin layout

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,17 @@
+const Product = require('../models/product.model');
+
 module.exports.signup = (req, res, next) => {
-    res.render('users/signup');
+    res.render('users/signup', {
+        title: "Sign Up"
+    });
+}
+
+module.exports.signupPost = (req, res, next) => {
+    
 }
 
 module.exports.signin = (req, res, next) => {
-    res.render('users/signin');
+    res.render('users/signin', {
+        title: 'Sign In'
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+    title: 'The kids world | Cửa hàng chuyên kinh doanh quần áo và đồ dùng trẻ em'
+  });
 });
 
 module.exports = router;
