0d07c042f09d2cd4aee645fa01ab6e2c0d9b556e
VoTan <vdt040499@gmail.com>
2020-06-14 21:36:22 +0700

    Display best seller products on Home page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const Product = require('../models/product.model');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    headTitle: 'The kids world | Cửa hàng chuyên kinh doanh quần áo và đồ dùng trẻ em'
+  Product.find((err, products) => {
+    if(err) {
+      console.log(err);
+    } else {
+      var bestSellerProducts = products.sort((a, b) => {
+        return b.sold - a.sold;
+      });
+      res.render('index', {
+        headTitle: 'The kids world | Cửa hàng chuyên kinh doanh quần áo và đồ dùng trẻ em',
+        bestSellerProducts: bestSellerProducts.slice(0, 8)
+      });
+    }
   });
+  
 });
 
 module.exports = router;
