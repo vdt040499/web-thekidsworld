3d2de368af75b59c6a0e86cd775f9d0d1d48ffa0
VoTan <vdt040499@gmail.com>
2020-06-14 18:04:39 +0700

    Admin Auth

--- routes/admin_categories.route.js
+++ routes/admin_categories.route.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../config/auth');
+const isAdmin = auth.isAdmin;
 
 const adminCategoriesController = require('../controllers/admin_categories.controller');
 
-router.get('/', adminCategoriesController.getCates);
-router.get('/add-category', adminCategoriesController.addCate);
+router.get('/', isAdmin, adminCategoriesController.getCates);
+router.get('/add-category', isAdmin, adminCategoriesController.addCate);
 router.post('/add-category', adminCategoriesController.addCatePost);
-router.get('/edit-category/:id', adminCategoriesController.editCate);
+router.get('/edit-category/:id', isAdmin,adminCategoriesController.editCate);
 router.post('/edit-category/:id', adminCategoriesController.editCatePost);
-router.get('/delete-category/:id', adminCategoriesController.deleteCate);
+router.get('/delete-category/:id', isAdmin, adminCategoriesController.deleteCate);
 
 module.exports = router;
