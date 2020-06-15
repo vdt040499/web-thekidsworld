a1c54683ed0668d3a825e3c9a9e55aa1da254cd4
VoTan <vdt040499@gmail.com>
2020-06-15 21:27:32 +0700

    Display cate products, best seller cate products on product detail And upload image in images file

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,48 +1,68 @@
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 
-const Category = require('../models/category.model');
-const Product = require('../models/product.model');
+const Category = require("../models/category.model");
+const Product = require("../models/product.model");
 
 //GET all products by category
 module.exports.getProductsByCategory = (req, res) => {
-    
-    var categorySlug = req.params.category;
+  var categorySlug = req.params.category;
 
-    Category.findOne({slug: categorySlug}, (err, cat) => {
-        Product.find({category: categorySlug}, (err, products) => {
-            res.render('product/cat_products', {
-                headTitle: cat.title,
-                products: products
-            });
-        });
-    }); 
-}
+  Category.findOne({ slug: categorySlug }, (err, cat) => {
+    Product.find({ category: categorySlug }, (err, products) => {
+      res.render("product/cat_products", {
+        headTitle: cat.title,
+        products: products,
+      });
+    });
+  });
+};
 
 //Get product details
 module.exports.getProductDetails = (req, res) => {
-    
-    var galleryImages = null;
+  var galleryImages = null;
 
-    Product.findOne({slug: req.params.product}, (err, product) => {
-        if(err) {
-            console.log(err);
+  Product.findOne({ slug: req.params.product }, (err, product) => {
+    if (err) {
+      console.log(err);
+    } else {
+      Product.find({ category: product.category }, (err, cateProducts) => {
+        if (err) {
+          console.log(err);
         } else {
-            var galleryDir = 'public/product_images/' + product._id + '/gallery';
+          var bestSellerCateProducts = cateProducts.sort((a, b) => {
+            return b.sold - a.sold;
+          });
 
-            fs.readdir(galleryDir, (err, files) => {
-                if(err) {
+          Product.find(
+            { category: product.category },
+            (err, categoryProducts) => {
+              if (err) {
+              } else {
+                var galleryDir =
+                  "public/product_images/" + product._id + "/gallery";
+
+                fs.readdir(galleryDir, (err, files) => {
+                  if (err) {
                     console.log(err);
-                } else {
+                  } else {
                     galleryImages = files;
-
-                    res.render('product/product_detail', {
-                        headTitle: product.title,
-                        p: product,
-                        galleryImages: galleryImages
+                    res.render("product/product_detail", {
+                      headTitle: product.name,
+                      p: product,
+                      galleryImages: galleryImages,
+                      cateProducts: categoryProducts.slice(0, 4),
+                      bestSellerCateProducts: bestSellerCateProducts.slice(
+                        0,
+                        4
+                      ),
                     });
-                }
-            })
+                  }
+                });
+              }
+            }
+          );
         }
-    });
-}
-
+      });
+    }
+  });
+};
